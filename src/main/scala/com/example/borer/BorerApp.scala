0d797b371e794e44d44dc8c6474b1c3db1bc978a
Saloni Vithalani <skvithalani@yahoo.com>
2020-02-18 15:40:35 +0530

    small refactoring

--- src/main/scala/com/example/borer/BorerApp.scala
+++ src/main/scala/com/example/borer/BorerApp.scala
@@ -8,14 +8,12 @@ import com.example.borer.model.Direction.North
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import scala.util.Try
 
 object BorerApp extends App {
   private val system: ActorSystem          = ActorSystem("test")
   private val serialization: Serialization = SerializationExtension(system)
 
-  private val lion             = Lion("lion")
-  private val zoo: Zoo         = message.Zoo(lion)
+  private val zoo: Zoo         = message.Zoo(Lion("lion"))
   private val compass: Compass = Compass(North)
 
   println("-------------------- Serialized Data ----------------------------")
@@ -26,20 +24,8 @@ object BorerApp extends App {
   println("")
   println("-------------------- Deserialized Data ----------------------------")
 
-  private val derializedZoo: Zoo =
-    serialization
-      .deserialize(serialization.serialize(zoo).get, classOf[Zoo])
-      .get
-  println(derializedZoo)
-
-  private val deserializedTiger: Try[Zoo] =
-    serialization.deserialize(
-      serialization.serialize(Zoo(Tiger)).get,
-      classOf[Zoo]
-    )
-
-  println(deserializedTiger.get)
-
+  println(serialization.deserialize(serialization.serialize(zoo).get, classOf[Zoo]).get)
+  println(serialization.deserialize(serialization.serialize(Zoo(Tiger)).get, classOf[Zoo]).get)
   println(serialization.deserialize(serialization.serialize(compass).get, classOf[Compass]).get)
 
   Await.result(system.terminate(), 5.seconds)
--- src/main/scala/com/example/jackson/JacksonApp.scala
+++ src/main/scala/com/example/jackson/JacksonApp.scala
@@ -3,19 +3,17 @@ package com.example.jackson
 import akka.actor.ActorSystem
 import akka.serialization.{Serialization, SerializationExtension}
 import com.example.jackson.message.{Compass, Zoo}
-import com.example.jackson.model.Animal.{Lion, Tiger}
+import com.example.jackson.model.Animal.Lion
 import com.example.jackson.model.Direction.North
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import scala.util.Try
 
 object JacksonApp extends App {
   private val system: ActorSystem          = ActorSystem("test")
   private val serialization: Serialization = SerializationExtension(system)
 
-  private val lion             = Lion("lion")
-  private val zoo: Zoo         = message.Zoo(lion)
+  private val zoo: Zoo         = message.Zoo(Lion("lion"))
   private val compass: Compass = Compass(North)
 
   println("-------------------- Serialized Data ----------------------------")
@@ -26,10 +24,7 @@ object JacksonApp extends App {
   println("")
   println("-------------------- Deserialized Data ----------------------------")
 
-  private val derializedZoo: Try[Zoo] =
-    serialization.deserialize(serialization.serialize(zoo).get, classOf[Zoo])
-  println(derializedZoo.get)
-
+  println(serialization.deserialize(serialization.serialize(zoo).get, classOf[Zoo]).get)
   println(serialization.deserialize(serialization.serialize(compass).get, classOf[Compass]).get)
 
   Await.result(system.terminate(), 5.seconds)
